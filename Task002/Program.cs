829577360fbb287e171d66ca7f93a865fa7563ee
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-27 19:33:57 +0300

    Task002 done

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -4,19 +4,12 @@ Console.WriteLine("Введите любое число от 10 до 999:");
 
 int a = int.Parse(Console.ReadLine());
 
-if (a > 999)
+if (a < 100)
 {
-    Console.WriteLine("Вы ввели слишком большое число, введите, пожалуйста, число не более 999");
+    Console.WriteLine("Вы ввели не трехзначное число, у которого нет третьей цифры");
 }
 else
 {
-    if (a > 99 & a < 1000)
-    {
-        int lastNumber = a % 10;
-        Console.WriteLine(lastNumber);
-    }
-    else
-    {
-        Console.WriteLine("Вы ввели не трехзначное число, у которого нет третьей цифры");
-    }
+    String str = a.ToString();
+    Console.WriteLine(str[2]);
 }
