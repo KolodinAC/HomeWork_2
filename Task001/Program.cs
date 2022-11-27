65a4055a0da3f07584a6bca234474aee84ec576a
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-27 19:23:24 +0300

    Task001 done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -4,7 +4,14 @@ Console.WriteLine("Введите любое трехзначное число:"
 
 int a = int.Parse(Console.ReadLine());
 
+if (a < 100 & a > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
 int firstNumber = a / 100;
 int result = (a - (firstNumber * 100))/10;
 
 Console.WriteLine(result);
+}
