9921f683f9d8aacac6fc68ae2fb8d2ddb78fa3f6
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-27 18:00:41 +0300

    Task001 done

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите любое трехзначное число:");
+
+int a = int.Parse(Console.ReadLine());
+
+int firstNumber = a / 100;
+int result = (a - (firstNumber * 100))/10;
+
+Console.WriteLine(result);
