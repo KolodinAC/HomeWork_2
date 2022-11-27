c0afd5b3a15544e4f6b26b9523a1b715c618419a
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-27 18:15:27 +0300

    Task002 done

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите любое число от 10 до 999:");
+
+int a = int.Parse(Console.ReadLine());
+
+if (a > 99 & a < 1000)
+{
+    int lastNumber = a % 10;
+    Console.WriteLine(lastNumber);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число, у которого нет третьей цифры");
+}
